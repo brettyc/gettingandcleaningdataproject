80cdc0c32203a14bf73d27c682155a3a8ad7a5fe
Brett <brettyc@gmail.com>
2016-08-06 12:20:00 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -9,7 +9,7 @@ library(dplyr)
   feature_names <- as.character(features[[2]])
   
   ##load the activity labels.  these will be joined with the data later
-  activity_labels <- read.table("activity_labels.txt", col.names = c("label_id", "label"))
+  activity_labels <- read.table("activity_labels.txt", col.names = c("activity_id", "activity"))
 
 ##load the test and training data
   ##load the test data set
@@ -18,22 +18,22 @@ library(dplyr)
   names(test_set) <- feature_names
   
   ##load the test labels and apply the column name 'label_id'
-  test_labels <- read.table("./test/y_test.txt", col.names = c("label_id"))
+  test_labels <- read.table("./test/y_test.txt", col.names = c("activity_id"))
   
   ##load the training data set
   train_set <- read.table("./train/X_train.txt")
   ##name the variables with feature names
   names(train_set) <- feature_names
   
-  ##load the training labels and apply the column name 'label_id'
-  train_labels <- read.table("./train/y_train.txt", col.names = c("label_id"))
+  ##load the training labels and apply the column name 'activity_id'
+  train_labels <- read.table("./train/y_train.txt", col.names = c("activity_id"))
 
 ##add the activity lable names to test_labels and also train_labels
   ##update test_labels to add the activity labels
-  test_labels <- inner_join(activity_labels, test_labels, by = "label_id")
+  test_labels <- inner_join(activity_labels, test_labels, by = "activity_id")
   
   ##update train_labels to add the activity labels
-  train_labels <- inner_join(activity_labels, train_labels, by = "label_id")
+  train_labels <- inner_join(activity_labels, train_labels, by = "activity_id")
   
   ##update test_set to add the activity labels
   test_set  <- bind_cols(test_labels, test_set)
@@ -57,10 +57,13 @@ library(dplyr)
 ##produced combined data set
   ##bind all rows for the test and training data together
   combined_data <- rbind(test_set, train_set)
-
-##subset combined_data to contain the subject, label, and only mean and std variables
-  subset_combined <- combined_data[,c("subject","label",grep("mean|std", names(combined_data), value = TRUE))]
   
+##melt the data for subject, activity, and only the variables with "mean" or "std" in their name
+  data_melt <- melt(combined_data, id.vars = c("subject", "activity"), measure.vars =grep("mean|std", names(combined_data)))
+
+##cast the data to get the average for each variable for each activity and each subject
+  data_cast <- cast(data_melt, subject + activity ~ variable, mean)
+
 ##write the combined_data to a .txt file
-  write.table(subset_combined, "tidy_data_set.txt", row.names = FALSE)
+  write.table(data_cast, "tidy_data_set.txt", row.names = FALSE)
   
